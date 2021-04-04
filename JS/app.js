7e8fc43324bfec24550cb9f2aa0560a474bf4ba6
Latifa Ghassan Abdul-Halim Amaireh <lalooshamayra@hotmail.com>
2021-04-04 16:24:01 +0300

    a art file

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+
+
+let userName = prompt("What\'s your name?");
+console.log (userName); 
+let userAge = prompt("What\'s your age?");
+console.log (userAge);
+let timeArt = prompt("How much time you spend in Art?");
+console.log (timeArt);
+let favArtist = prompt("Who is your favourite artist?");
+console.log (favArtist);
+
+alert(' Your name is '  +    userName +  ' your age is '  +  userAge  + ' you spend ' +  timeArt   + ' in art ' + ' your favourite artist is '  +  favArtist );
