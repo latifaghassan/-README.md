4f9ba42bfc76ee8dc71d8dc2655ff63086411617
Latifa Ghassan Abdul-Halim Amaireh <lalooshamayra@hotmail.com>
2021-04-04 16:46:10 +0300

    add the typeof

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,12 @@
 
 
 let userName = prompt("What\'s your name?");
-console.log (userName); 
+console.log (userName,typeof userName); 
 let userAge = prompt("What\'s your age?");
-console.log (userAge);
+console.log (userAge,typeof userAge);
 let timeArt = prompt("How much time you spend in Art?");
-console.log (timeArt);
+console.log (timeArt,typeof timeArt);
 let favArtist = prompt("Who is your favourite artist?");
-console.log (favArtist);
+console.log (favArtist,typeof favArtist);
 
 alert(' Your name is '  +    userName +  ' your age is '  +  userAge  + ' you spend ' +  timeArt   + ' in art ' + ' your favourite artist is '  +  favArtist );
